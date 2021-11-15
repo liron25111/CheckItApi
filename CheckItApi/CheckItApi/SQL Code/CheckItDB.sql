cd510d83386ac32aa9eeffe978b3bc8e3c0167e2
User <User@LAB3-10>
2021-11-15 13:59:12 +0200

    q

--- CheckItApi/CheckItApi/SQL Code/CheckItDB.sql
+++ CheckItApi/CheckItApi/SQL Code/CheckItDB.sql
@@ -2,7 +2,8 @@ Use Confirmation
 go
 
 
-
+INSERT INTO Account (Username, Pass,Email)
+VALUES ('liron','2511','liron.abovich@gmail.com');
 
 INSERT INTO Account (Username, Pass,Email)
 VALUES ('Yaniv','1234','yanivp1414@gmail.com');
